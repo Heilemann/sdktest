eb06c99f7f1e68b4b8d928fe89db80500d3dfb29
Michael Heilemann <michael.heilemann@me.com>
2022-07-29 18:01:12 -0400

    Show proof

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
+				<p className='App-logo'>{document.current?._id}</p>
 			</header>
 		</div>
 	)
