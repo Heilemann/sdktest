230472a9a3e3c1e99e4308227d87ae6f95f9323d
Michael Heilemann <michael.heilemann@me.com>
2022-08-01 09:53:41 -0400

    commit

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -19,24 +19,65 @@ export default function Character(props: ICharacterProps) {
 
 	return (
 		<div className='space-y-2'>
-			<img
-				src={require('../assets/coclogo.png')}
-				alt='Call of Cthulhu Logo'
-				className='m-auto mt-2 mb-6 max-w-xs'
-			/>
+			<div className='flex flex-col md:flex-row md:space-x-10'>
+				<div
+					className='m-auto mt-2 mb-4 grid h-20 w-full place-items-center bg-cover md:mb-0 md:h-28 md:w-60'
+					style={{
+						background:
+							'url(' + require('../assets/coclogo.png') + ') no-repeat center',
+						backgroundSize: 'contain',
+					}}
+				/>
 
-			<div className='flex space-x-2'>
-				<Label className='w-32 self-center' htmlFor='name'>
-					Name
-				</Label>
-				<Input placeholder='Name...' {...register('name')} />
+				<div className='flex-1 flex-col space-y-2 self-center'>
+					<div className='flex space-x-2'>
+						<Label className='flex-0 w-32 self-center' htmlFor='name'>
+							Name
+						</Label>
+						<Input
+							className='flex-1'
+							placeholder='Name...'
+							{...register('name')}
+						/>
+					</div>
+
+					<div className='flex space-x-2'>
+						<Label className='flex-0 w-32 self-center' htmlFor='occupation'>
+							Occupation
+						</Label>
+						<Input
+							className='flex-1'
+							placeholder='Occupation...'
+							{...register('occupation')}
+						/>
+					</div>
+
+					<div className='flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0'>
+						<div className='flex space-x-2'>
+							<Label className='flex-0 w-32 self-center' htmlFor='gender'>
+								Gender
+							</Label>
+							<Input
+								className='flex-1'
+								placeholder='Gender...'
+								{...register('gender')}
+							/>
+						</div>
+
+						<div className='flex space-x-2'>
+							<Label className='flex-0 w-32 self-center md:w-10' htmlFor='age'>
+								Age
+							</Label>
+							<Input
+								className='flex-1'
+								placeholder='Age...'
+								{...register('age')}
+							/>
+						</div>
+					</div>
+				</div>
 			</div>
-			<div className='flex space-x-2'>
-				<Label className='w-32 self-center' htmlFor='occupation'>
-					Occupation
-				</Label>
-				<Input placeholder='Occupation...' {...register('occupation')} />
-			</div>
+
 			<SectionDivider>Abilities</SectionDivider>
 			<div className='flex space-x-2'>
 				<div className='space-y-1'>
