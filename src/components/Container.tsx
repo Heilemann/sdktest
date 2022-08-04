8c4317277cf7bbff69dc7909807ad5192e02b961
Michael Heilemann <michael.heilemann@me.com>
2022-08-04 18:31:32 -0400

    commit

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,10 +1,7 @@
 import { useContext, useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { TDocument } from '../interfaces'
-import Character from './Character'
 import context from './context'
-import Note from './Note'
-import Scene from './Scene'
 
 export interface IContainerProps {}
 
@@ -112,11 +109,11 @@ export default function Container(props: IContainerProps) {
 	return (
 		<div className='flex h-full flex-col bg-white p-4 text-sm text-gray-900 dark:bg-gray-900 dark:text-gray-100'>
 			<div className='flex-1'>
-				{type === 'character' && <Character />}
+				{/* {type === 'character' && <Character />}
 				{type === 'note' && <Note />}
 				{type === 'scene' && (
 					<Scene setValue={setValue} messageToApp={messageToApp} />
-				)}
+				)} */}
 			</div>
 		</div>
 	)
