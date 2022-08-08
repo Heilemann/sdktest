853e563b100d24f7fcbe7d5a87450e7a588ac1fa
Michael Heilemann <michael.heilemann@me.com>
2022-08-08 18:00:58 -0400

    commit

--- src/components/Asset.tsx
+++ src/components/Asset.tsx
@@ -20,6 +20,8 @@ const Asset: FC<AssetProps> = props => {
 	const assetId = document.values[name]
 	const asset = assets.find((asset: TAsset) => asset._id === assetId)
 
+	console.log('---> asset looking up assetId:', assetId, assets, asset)
+
 	// should move this to a context
 	let parentOrigin = ''
 	// if (process.env.NODE_ENV === 'development') {
