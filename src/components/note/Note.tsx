fe43d16c2d4a4f35ec61c1f96c8775dfe2258f53
Michael Heilemann <michael.heilemann@me.com>
2022-08-12 16:28:53 -0400

    commit

--- src/components/note/Note.tsx
+++ src/components/note/Note.tsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react'
 import { useFormContext } from 'react-hook-form'
+import { twMerge } from 'tailwind-merge'
 import context from '../context'
 import Input from '../Input'
 import TextArea from '../Textarea'
@@ -14,27 +15,29 @@ export default function Note(props: INoteProps) {
 
 	if (!document?.values) return null
 
-	if (editMode === 'view') {
-		return (
-			<div>
-				<div className='mb-4 font-bold'>{values.name}</div>
-				<div>{values.note}</div>
-			</div>
-		)
-	} else {
-		return (
-			<>
-				<Input
-					className='flex-0 font-bold'
-					placeholder='Name...'
-					{...register('name')}
-				/>
-				<TextArea
-					className='m-0 mt-2 flex-1 resize-none'
-					placeholder='Note...'
-					{...register('note')}
-				/>
-			</>
-		)
-	}
+	return (
+		<>
+			{editMode === 'view' && (
+				<div>
+					<div className='mb-4 font-bold'>{values.name}</div>
+					<div>{values.note}</div>
+				</div>
+			)}
+
+			<Input
+				className={twMerge('flex-0 font-bold', editMode === 'view' && 'hidden')}
+				placeholder='Name...'
+				{...register('name')}
+			/>
+
+			<TextArea
+				className={twMerge(
+					'm-0 mt-2 flex-1 resize-none',
+					editMode === 'view' && 'hidden',
+				)}
+				placeholder='Note...'
+				{...register('note')}
+			/>
+		</>
+	)
 }
