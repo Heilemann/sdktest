545573396e79fc4ec98add05e58bcf4981869ad2
Michael Heilemann <michael.heilemann@me.com>
2022-08-12 14:15:57 -0400

    commit

--- src/components/character/Characteristics.tsx
+++ src/components/character/Characteristics.tsx
@@ -7,7 +7,7 @@ export default function Characteristics(props: ICharacteristicsProps) {
 	const { register } = useFormContext()
 
 	return (
-		<div className='grid grid-cols-3 md:grid-cols-5 flex-1 gap-2 mt-2'>
+		<div className='grid grid-cols-5 sm:grid-cols-5 flex-1 gap-2 mt-2'>
 			<Characteristic label='STR' {...register('characteristics.strength')} />
 			<Characteristic label='DEX' {...register('characteristics.dexterity')} />
 			<Characteristic
